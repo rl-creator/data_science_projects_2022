209ec555f5a19b828700f9535274c4791154ab10
rl-creator <ryan.leeson@ucalgary.ca>
2022-01-27 11:55:07 +0000

    First commit. Adding notebook and first module.

---
+++ cifar10_image_classification/modules/import_data.py
@@ -0,0 +1,15 @@
+
+'''Module containing function to import dataset.'''
+
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+
+def get_data ():
+    '''Import included cifar10 dataset in the tensorflow kears library.'''
+    return cifar10.load_data ()
+
+def get_train_test_data (data):
+    (X_train, y_train), (X_test, y_test) = data
+    return X_train, y_train, X_test, y_test
+
+#if __name__ == "__main__":
