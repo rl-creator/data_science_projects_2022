ba223affa08ec21e7c4cfb9628ca5f94d391c7a3
rl-creator <ryan.leeson@ucalgary.ca>
2022-01-28 12:07:09 +0000

    Update documentation for storage.py module.

--- cifar10_image_classification/modules/storage.py
+++ cifar10_image_classification/modules/storage.py
@@ -1,13 +1,17 @@
 
-'''Functions to produce data structures to help with the storage of outputs
-from functions.'''
+'''Function to create data structures for better organisation of output 
+from functions.
+This makes it easier to understand what is output from a function, as the 
+outputs can be identified from given labels, and as a result, it is easier to 
+identify what is being input into a subsequent function.
+'''
 
 from collections import namedtuple
 
 def store_data (*function_output):
-    '''Create a namedtuple to store output from a function.
-    Namedtuples are used for labelling output of a function and more easily 
-    organise the input in the following function.
+    '''Creates a namedtuple to organise output from a function.
+    Namedtuples are used because the function's output can be labeled making 
+    it easier to identify and understand what was output.
     '''
     return namedtuple ('storage', function_output)
 
