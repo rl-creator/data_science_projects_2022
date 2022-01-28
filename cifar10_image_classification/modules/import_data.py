763b276e19916ddec5c04553b6abd70a2377867b
rl-creator <ryan.leeson@ucalgary.ca>
2022-01-28 12:52:13 +0000

    Added store_data from storage.py to get_train_test_data from import_data.py. And tested.

--- cifar10_image_classification/modules/import_data.py
+++ cifar10_image_classification/modules/import_data.py
@@ -4,6 +4,7 @@ response variables.'''
 
 import tensorflow as tf
 from tensorflow.keras.datasets import cifar10
+from storage import store_data
 
 def get_data ():
     '''Import included cifar10 dataset in the tensorflow kears library.'''
@@ -12,7 +13,7 @@ def get_data ():
 def get_train_test_data (data):
     '''Unpack data into X and y training and test sets.'''
     (X_train, y_train), (X_test, y_test) = data
-    return X_train, y_train, X_test, y_test
+    return store_data ('X_train', 'y_train', 'X_test', 'y_test') (X_train, y_train, X_test, y_test)
 
 if __name__ == "__main__":
     pass
