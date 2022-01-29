d75dc7cc5e71efaa741ce1ba15fc1cd80f28e4b9
rl-creator <ryan.leeson@ucalgary.ca>
2022-01-29 11:23:54 +0000

    Corrected data normalisation. y sets do not need normalisation.

--- cifar10_image_classification/modules/import_data.py
+++ cifar10_image_classification/modules/import_data.py
@@ -21,9 +21,14 @@ def normalise_data (train_test_data):
     The RBG colour scale for photos measures red, blue, and green on a 0 to 255 
     point scale. Dividing by 255 coverts all RBG measurements to a 0 to 1 scale.
     '''
-    data_normalised = tuple (arr.astype (float) /255 for arr in train_test_data)
+    X_train_norm = train_test_data.X_train /255
+    X_test_norm = train_test_data.X_test /255
+    y_train = train_test_data.y_train
+    y_test = train_test_data.y_test
 
-    train_test_data_norm = store_data ('X_train_norm', 'y_train_norm', 'X_test_norm', 'y_test_norm') (*data_normalised)
+    data_normalised = (X_train_norm, y_train, X_test_norm, y_test)
+
+    train_test_data_norm = store_data ('X_train_norm', 'y_train', 'X_test_norm', 'y_test') (*data_normalised)
     
     return store_data ('data_raw', 'data_normalised') (train_test_data, train_test_data_norm)
 
